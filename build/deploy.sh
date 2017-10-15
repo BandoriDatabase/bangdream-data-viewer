1d0a4994baaaf1759931ba3cbd38eb58edb1f8b4
DNARoma <tangpei506@gmail.com>
2017-10-15 22:48:09 +0200

    add delete-after for all server

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -r --quiet $TRAVIS_BUILD_DIR/dist root@bangdream.ga:/var/www/html/
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist root@bangdream.ga:/var/www/html/
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist root@bangdream.ga:/var/www/html/
