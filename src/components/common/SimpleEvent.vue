a3b13ed15820087b8242f46ce95e582f4cbe301b
DNARoma <tangpei506@gmail.com>
2017-11-13 00:19:59 +0100

    add missing files

---
+++ src/components/common/SimpleEvent.vue
@@ -0,0 +1,114 @@
+<template>
+  <lazy-component @show="loadData">
+    <q-card v-if="isReady">
+      <q-card-title class="bg-pink text-white">
+        {{currentEvent[server].eventName}}
+        <span slot="subtitle" class="text-white">{{$t('event-' + server)}} {{currentEvent[server].eventType}}</span>
+      </q-card-title>
+      <q-card-media>
+        <img v-if="server === 'jp'" class="responsive" v-lazy="`/assets-jp/homebanner_banner_event${currentEvent[server].eventId}.png`"/>
+        <img v-if="server === 'tw'" class="responsive" v-lazy="`/assets-tw/homebanner_banner_event${padEventId(currentEvent[server].eventId)}_open.png`"/>
+      </q-card-media>
+      <q-card-main>
+        <div class="row items-center justify-center">
+          <div class="col-3"><card-thumb :cardId="Number(eventNormalCardId)" :server="server"></card-thumb></div>
+          <div class="col-3"><card-thumb :cardId="Number(eventSpecialCardId)" :server="server"></card-thumb></div>
+          <count-down class="col-6" :target-time="Number(currentEvent[server].endAt)" v-if="Number(currentEvent[server].endAt) > Date.now()" small></count-down>
+        </div>
+      </q-card-main>
+    </q-card>
+    <q-card v-else>
+      <q-card-title class="bg-pink text-white">
+        {{$t('fetch-event-data')}}
+        <span slot="subtitle" class="text-white">{{$t('fetch-event-data')}}</span>
+      </q-card-title>
+      <q-card-main>
+        <q-spinner color="pink" size="48px"></q-spinner>
+      </q-card-main>
+    </q-card>
+  </lazy-component>
+</template>
+
+<i18n>
+{
+  "en": {
+    "event-jp": "Event JP",
+    "event-tw": "Event TW",
+    "fetch-event-data": "Fetching Event Data..."
+  },
+  "zh-CN": {
+    "event-jp": "日服活动",
+    "event-tw": "台服活动",
+    "fetch-event-data": "获取活动数据中"
+  },
+  "zh-TW": {
+    "event-jp": "日服活動",
+    "event-tw": "台服活動",
+    "fetch-event-data": "獲取活動數據中"
+  }
+}
+</i18n>
+
+<script>
+import {
+  QCard,
+  QCardTitle,
+  QCardMedia,
+  QCardMain,
+  QIcon,
+  QBtn,
+  QSpinner
+} from 'quasar'
+import { mapState, mapActions } from 'vuex'
+import CountDown from './Countdown'
+import cardThumb from './CardThumb'
+
+export default {
+  name: 'simple-event-card',
+  props: ['server'],
+  data () {
+    return {
+      isReady: false,
+      eventNormalCardId: null,
+      eventSpecialCardId: null
+    }
+  },
+  components: {
+    QCard,
+    QCardTitle,
+    QCardMedia,
+    QCardMain,
+    QIcon,
+    QBtn,
+    QSpinner,
+    CountDown,
+    cardThumb
+  },
+  computed: {
+    ...mapState('event', [
+      'currentEvent'
+    ])
+  },
+  methods: {
+    ...mapActions('event', [
+      'getCurrentEvent'
+    ]),
+    padEventId (evtId) {
+      return String(evtId).length === 1 ? `0${String(evtId)}` : String(evtId)
+    },
+    loadData () {
+      this.$nextTick(async () => {
+        await this.getCurrentEvent(this.server)
+        const eventCards = this.currentEvent[this.server].pointRewards.filter(elem => elem.rewardType === 'situation')
+        this.eventNormalCardId = eventCards[0].rewardId
+        this.eventSpecialCardId = eventCards[1].rewardId
+        this.isReady = true
+      })
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/components/common/SimpleGacha.vue
@@ -0,0 +1,128 @@
+<template>
+  <lazy-component @show="loadData">
+    <q-card v-if="isGcahaReady">
+      <q-card-title class="bg-pink text-white">
+        {{$t(`gache-list-${server}`)}}
+        <span slot="subtitle" class="text-white">{{$t('gacha-list-count')}}: {{currentGachaList[server].length}}</span>
+      </q-card-title>
+      <q-card-main>
+        <div v-for="gacha in currentGachaList[server]" :key="gacha.gachaId" style="width: 100%; margin: 10px 0; text-align: center;">
+          <img v-lazy="`/assets-${server}/gacha/screen/${gacha.resourceName}_logo.png`" alt="" class="gacha-banner" />
+          <p>{{gacha.gachaName}}<q-btn flat round small class="text-pink" @click="$refs.gachaModal.open(gacha)"><q-icon name="launch" /></q-btn></p>
+          <count-down :target-time="Number(gacha.closedAt)"></count-down>
+        </div>
+        <gacha-modal ref="gachaModal" :server="server"></gacha-modal>
+      </q-card-main>
+    </q-card>
+    <q-card v-else>
+      <q-card-title class="bg-pink text-white">
+        {{$t('fetch-gacha-data')}}
+        <span slot="subtitle" class="text-white">{{$t('gacha-list-count')}} {{$t('fetch-gacha-data')}}</span>
+      </q-card-title>
+      <q-card-main>
+        <q-spinner color="pink" size="48px"></q-spinner>
+      </q-card-main>
+    </q-card>
+  </lazy-component>
+</template>
+
+<i18n>
+{
+  "en": {
+    "gacha-end-cd": "Gacha ending countdown",
+    "gache-list-jp": "JP scouts list",
+    "gache-list-tw": "TW scouts list",
+    "gacha-list-count": "Counts",
+    "fetch-gacha-data": "Fetching Gacha Data..."
+  },
+  "zh-CN": {
+    "gacha-end-cd": "抽卡倒计时",
+    "gache-list-jp": "日服当前开放扭蛋池",
+    "gache-list-tw": "台服当前开放扭蛋池",
+    "gacha-list-count": "数量",
+    "fetch-gacha-data": "获取扭蛋池数据中"
+  },
+  "zh-TW": {
+    "gacha-end-cd": "抽卡倒計時",
+    "gache-list-jp": "日服當前開放扭蛋池",
+    "gache-list-tw": "臺服當前開放扭蛋池",
+    "gacha-list-count": "數量",
+    "fetch-gacha-data": "獲取扭蛋池數據中"
+  }
+}
+</i18n>
+
+<script>
+import {
+  QCard,
+  QCardTitle,
+  QCardMedia,
+  QCardMain,
+  QIcon,
+  QBtn,
+  QSpinner
+} from 'quasar'
+import { mapState, mapActions } from 'vuex'
+import gachaModal from '../modals/Gacha'
+import CountDown from './Countdown'
+
+export default {
+  name: 'simple-gacha-card',
+  props: ['server'],
+  components: {
+    gachaModal,
+    QCard,
+    QCardTitle,
+    QCardMedia,
+    QCardMain,
+    QIcon,
+    QBtn,
+    CountDown,
+    QSpinner
+  },
+  data () {
+    return {
+      isGcahaReady: false
+    }
+  },
+  computed: {
+    ...mapState('gacha', [
+      'currentGachaList'
+    ])
+  },
+  methods: {
+    ...mapActions('gacha', [
+      'getGachaCurrent'
+    ]),
+    loadData () {
+      this.$nextTick(async () => {
+        await this.getGachaCurrent(this.server)
+        this.isGcahaReady = true
+      })
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.gacha-banner
+  width 280px
+  height 120px
+  margin 0 auto
+  max-width 100%
+  display block
+  
+.gacha-cover
+  width: 100%
+  height: 200px
+  background-size: 100% 100%
+  background-repeat: no-repeat
+  background-position: center
+
+.gacha-img
+  width: 100%
+  height: 500px
+  background-size: auto 100%
+  background-repeat: no-repeat
+  background-position: center
+</style>
