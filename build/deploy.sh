796b47f77916f861e2098a4569247121723408df
DNARoma <tangpei506@gmail.com>
2018-02-14 22:59:45 +0800

    update hk mirror deploy address

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+ssh-keyscan -t rsa $DEPLOY_HOST_GLOBAL >> ~/.ssh/known_hosts
+ssh-keyscan -t rsa $DEPLOY_HOST_HK >> ~/.ssh/known_hosts
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist $DEPLOY_USER@$DEPLOY_HOST_GLOBAL:$DEPLOY_PATH
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist $DEPLOY_USER@$DEPLOY_HOST_HK:$DEPLOY_PATH
