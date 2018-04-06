cd97da1f0e39ca20740407da3f252098e052a81a
DNARoma <tangpei506@gmail.com>
2018-04-07 00:30:58 +0200

    fix update note wrong display

--- src/components/modals/update-notes.vue
+++ src/components/modals/update-notes.vue
@@ -2,11 +2,11 @@
   <q-modal ref="modal" :content-css="{maxWidth: '800px', padding: '10px'}"
     v-model="isOpen"
   >
-    <h4 class="uppercase">{{$t('update.title')}} <small>0.5.0</small></h4>
+    <h4 class="uppercase">{{$t('update.title')}} <small>0.6.0</small></h4>
     <p>{{$t('update.desc')}}</p>
     <h5 class="uppercase">{{$t('update.new-features')}}</h5>
     <ul>
-      <li v-for="i in 11" :key="i">{{$t(`update.feature-detail[${i - 1}]`)}}</li>
+      <li v-for="i in 2" :key="i">{{$t(`update.feature-detail[${i - 1}]`)}}</li>
     </ul>
     <h5 class="uppercase">{{$t('update.fixed-bugs')}}</h5>
     <ul>
