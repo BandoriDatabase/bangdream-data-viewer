ab067b06f816ad071e172e545515520bc901a482
thtl1999 <thtl2004@hanmail.net>
2018-04-11 18:38:46 +0900

    Updated kr.js
    tried to use same words as Korean server as much as possible
    translated only what i can see in web
    grammar checked, i think it is the best i can

--- src/i18n/kr.js
+++ src/i18n/kr.js
@@ -5,7 +5,7 @@ export default {
     'card': '카드',
     'table': '테이블',
     'gallery': '갤러리',
-    'music': '노래',
+    'music': '음악',
     'SFC': '로딩 만화',
     'about': '정보',
     'secTitle': 'Building info',
@@ -38,12 +38,12 @@ export default {
     ],
     'welcome-bug-report': "If you find any problem affecting you use this web app, please report it to me per <a href='mailto:dev@bangdream.ga'>Email</a>, on <a href='https://dnaroma.site/2017/12/23/bug-report/'>Bug report page</a> or on <a href='https://discord.gg/vGb3eHH'>Discord</a>."
   },
-  'event-srv': 'Event {srv}',
+  'event-srv': '{srv} 이벤트',
   'fetch-data': '{type} 데이터 가져오는중...',
-  'not-started': 'Not started until',
+  'not-started': '이벤트 시작 시간',
   'common': {
     'event': '이벤트',
-    'gacha': '가챠',
+    'gacha': '뽑기',
     'jp': '일본 서버',
     'tw': '대만 서버',
     'kr': '한국 서버',
@@ -51,7 +51,7 @@ export default {
     'perform': '퍼포먼스',
     'technic': '테크닉',
     'visual': '비쥬얼',
-    'total': '총합력',
+    'total': '종합력',
     'skill': '스킬',
     'avatar': '아바타',
     'character': '캐릭터',
@@ -60,34 +60,34 @@ export default {
     'filter': '찾기',
     'sort': {
       'title': '정렬 설정',
-      'asc': '낮은 순 정렬',
-      'desc': '높은 순 정렬'
+      'asc': '오름차순 정렬',
+      'desc': '내림차순 정렬'
     },
     'band': '밴드',
     'difficulty': '난이도',
-    'apply-save': '저장 & 적용',
+    'apply-save': '저장',
     'data-ver': 'Data Ver',
     'app-ver': 'App Ver'
   },
   'gacha': {
     'list-title': '{srv} Scouts List',
     'list-count': 'Counts',
-    'start-at': 'Start at:',
-    'end-at': 'Close at:"',
-    'desc': 'Description',
-    'new-members': 'New members',
-    'rates': '뽑기 확률',
-    'pickup': '출현율UP 카드 목록',
-    'normal': '출현 카드 목록',
+    'start-at': '시작:',
+    'end-at': '종료:',
+    'desc': '정보',
+    'new-members': '신 멤버',
+    'rates': '출현율',
+    'pickup': '출현율UP 멤버 목록',
+    'normal': '출현 멤버 목록',
     'open-detail': '자세한 정보 열기'
   },
   'mobile': {
-    'click-collapsible': 'Click one of the server name to expand'
+    'click-collapsible': '원하는 서버의 이름을 누르면 펼쳐집니다'
   },
   'card': {
     'name-display': [
       '이름 표기법 바꾸기: ',
-      '영어',
+      '알파벳',
       '일본어'
     ],
     'attr': {
@@ -100,25 +100,25 @@ export default {
     'un-trained': '특훈 전/후',
     'cut-in-normal': '배경 포함/미포함',
     'live-chara': 'SD 캐릭터',
-    'self-intro-unlock-reward': '에피소드 해금',
-    'max-lv-unlock-reward': '메모리얼 해금',
+    'self-intro-unlock-reward': '에피소드 오픈 보너스',
+    'max-lv-unlock-reward': '메모리얼 오픈 보너스',
     'skill-title': '스킬 정보',
     'skill-level': '스킬 레벨',
-    'training-title': '특훈 필요 재료',
+    'training-title': '특훈 필요 아이템',
     'no-train-ava': '특훈 불가능',
-    'story': '카드 스토리',
+    'story': '카드 에피소드',
     'story-self-intro': '에피소드: ',
-    'story-to-unlock': '필요 재료:',
+    'story-to-unlock': '필요 아이템:',
     'story-max-level': '메모리얼 에피소드: ',
     'story-none': '스토리 없음',
-    'story-reward': '스토리 해금 보상'
+    'story-reward': '에피소드 달성 보너스 보상'
   },
   'music': {
     'select-band': '밴드 선택',
     'composer': '작곡',
     'lyricist': '작사',
     'arranger': '편곡',
-    'howtoget': '해금 방법',
+    'howtoget': '획득 방법',
     'difficulty': '난이도',
     'achieve': '달성 보상',
     'combo': '총 노트 수',
@@ -132,16 +132,16 @@ export default {
     'auto-play-with-bgm': 'BGM과 함께 자동 재생',
     'stop': '중지',
     'show-bg-image': '배경 이미지',
-    'stop-hint-text': '재생이 끝날 때 까지 기다려주세요...',
-    'curr-voice': '재생중'
+    'stop-hint-text': '보이스 재생이 끝날 때까지 기다려주세요...',
+    'curr-voice': '재생 중'
   },
   'live2d': {
     'costum': '의상',
     'voice': '대사',
-    'show': '보기',
+    'show': '적용',
     'how-to-use': '사용법',
     'costum-name': '의상:',
-    'costum-how-to-get': '해금 방법:',
+    'costum-how-to-get': '오픈 조건:',
     'costum-avaliable-from': '출시일:',
     'voice-appear': '대사 출처:',
     'voice-type': '대사 타입:',
@@ -152,17 +152,17 @@ export default {
     }
   },
   'event': {
-    'title': '이벤트 {srv}',
-    'type': '이벤트 타입:',
-    'start-cd': 'Event starting countdown',
-    'end-cd': 'Event ending countdown',
+    'title': '{srv} 이벤트',
+    'type': '이벤트 타입: ',
+    'start-cd': '남은 시작시간',
+    'end-cd': '남은 진행기간',
     'dist-cd': 'Event award distribution countdown',
     'next-cd': 'Next event countdown',
-    'reward-card': 'Reward cards',
-    'reward-stamp': 'Reward stamp',
-    'bonus-attr-card': 'Bonus attribute and cards',
-    'badge': 'Event badge',
-    'degrees': 'Degrees preview',
-    'musics': 'Event musics'
+    'reward-card': '보상 멤버',
+    'reward-stamp': '보상 레어 스탬프',
+    'bonus-attr-card': '이벤트 보너스 대상',
+    'badge': '이벤트 뱃지',
+    'degrees': '이벤트 칭호',
+    'musics': '이벤트 음악들'
   }
 }
