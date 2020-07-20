2c92a6b15ccae94c355be67ff1c20a26875ecce4
DNARoma <tangpei506@gmail.com>
2020-07-20 12:19:11 +0200

    migrate to quasar 1.12

--- babel.config.js
+++ babel.config.js
@@ -26,6 +26,7 @@ module.exports = {
     [
       'transform-imports', {
         'quasar': {
+          'transform': require('quasar/dist/babel-transforms/imports.js'),
           'preventFullImport': true
         }
       }
